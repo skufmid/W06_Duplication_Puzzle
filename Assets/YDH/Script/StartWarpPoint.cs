789d50b4cd1d8e4e7b88b123fcc176e3ad4d5b86
6767cc <116047063+6767cc@users.noreply.github.com>
2025-04-12 01:52:45 +0900

    Reapply "[feat] 임시 포탈 스크립트 및 그것을 이용한 스테이지"
    This reverts commit 196aef34f866d597460a479d29b0f41c45e324fd.

---
+++ Assets/YDH/Script/StartWarpPoint.cs
@@ -0,0 +1,17 @@
+using UnityEditor.ProjectWindowCallback;
+using UnityEngine;
+
+public class StartWarpPoint : MonoBehaviour
+{
+    public GameObject EndWarpPoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("¹«¾ð°¡ ´êÀ½");
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.position = EndWarpPoint.transform.position;
+        }
+    }
+}
