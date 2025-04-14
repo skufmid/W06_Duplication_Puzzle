f8800f389ce3f49214d7504964404eeb87d86e68
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-04-14 21:46:21 +0900

    [Feat] 프리팹 오브젝트 Order In Layer 조정

---
+++ Assets/Scripts/OrderInLayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class OrderInLayer : MonoBehaviour
+{
+    private SpriteRenderer _renderer;
+
+    private void Awake()
+    {
+        _renderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        SetOrderInLayer();
+    }
+
+    private void SetOrderInLayer()
+    {
+        _renderer.sortingOrder = 30 - 2 * (int)transform.position.y - 1;
+    }
+}
