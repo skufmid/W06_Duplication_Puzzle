2b09e400348fd6ec3324768efa609671c83c51e4
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-04-14 11:05:19 +0900

    [Feat] 아래에 있는 플레이어, 장난감 병정이 더 앞에 보이게 & 프리펩에 안 들어가 있던 오류 수정

--- Assets/Scripts/ToySoldier.cs
+++ Assets/Scripts/ToySoldier.cs
@@ -3,7 +3,17 @@
 public class ToySoldier : MonoBehaviour
 {
     public int ToySoldierId;
+    private SpriteRenderer _renderer;
 
+    private void Awake()
+    {
+        _renderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        SetOrderInLayer();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -13,5 +23,10 @@ public class ToySoldier : MonoBehaviour
                 Destroy(collision.gameObject);
             }
         }
+    }
+
+    private void SetOrderInLayer()
+    {
+        _renderer.sortingOrder = 30 - 2 * (int)transform.position.y - 1;
     }
 }
