a6767f67061ce2acc8a004378905d1539fdba5af
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-04-14 16:17:24 +0900

    [Feat] 랜덤한 장미가시 이미지 랜더링

--- Assets/Scripts/DeathGround.cs
+++ Assets/Scripts/DeathGround.cs
@@ -2,11 +2,22 @@
 
 public class DeathGround : MonoBehaviour
 {
+    SpriteRenderer _renderer;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Destroy(collision.gameObject);
         }
+    }
+
+    private void Awake()
+    {
+        _renderer = GetComponentInChildren<SpriteRenderer>();
+        int randomIndex = Random.Range(0, 3);
+        _renderer.sprite = GameManager.Instance.RoseVines[randomIndex];
+        randomIndex = Random.Range(0, 2);
+        _renderer.flipX = randomIndex == 1 ? true : false;
     }
 }
